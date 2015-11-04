03a66eb382596bf1ebf08376e6de77688fb47d79
xubing <xubinghku@gmail.com>
2015-11-04 11:08:45 +0800

    3 sum closest

--- threeSum1.py
+++ threeSum1.py
@@ -1,3 +1,4 @@
+'''
 Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
 Note:
@@ -9,9 +10,10 @@ For example, given array S = {-1 0 1 2 -1 -4},
     A solution set is:
     (-1, 0, 1)
     (-1, -1, 2)
-
+'''
 
 '''
+
 Be careful about
 1. I could remove the duplicates at the beginning, or at the end. But the latter is faster.
 2. Different from twoSum, finish the loop to find all possible combinations.
