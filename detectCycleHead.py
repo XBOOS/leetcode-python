f5c6f751981d55bfd17316192ee1d2a374de10d0
xubing <xubinghku@gmail.com>
2015-11-04 18:19:07 +0800

    detect the start of cycle

---
+++ detectCycleHead.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        s = set()
+        while head !=None:
+            if head in s:
+                return head
+            s.add(head)
+            head = head.next
+        return None    
+
+#Method2 with slow and fast pointers
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow==fast:
+                while head!=slow:
+                    head = head.next
+                    slow = slow.next
+                return slow
+                
+            
+        return None  
