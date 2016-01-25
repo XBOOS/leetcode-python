3ecf86bb87a018fdf42b6c71870a9ff5aa431347
xubing <xubinghku@gmail.com>
2016-01-25 11:14:06 +0800

    Jan 25 2016

--- merge_sorted_arrays_inPlace.py
+++ merge_sorted_arrays_inPlace.py
@@ -32,3 +32,21 @@ class Solution(object):
                 nums1[k] = nums2[jj]
                 k+=1
 
+#Method2  smart solution
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        while n>0:
+            if m>0 and nums1[m-1]>=nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -=1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -=1
+
