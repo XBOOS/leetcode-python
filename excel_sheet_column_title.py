d5345e46f81ba273644491398d0d5bc8fe7a6256
xubing <xubinghku@gmail.com>
2016-02-02 08:53:41 +0800

    Feb 2 2016

---
+++ excel_sheet_column_title.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB """
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        res = ""
+        while n>0:
+            res =chr(65+(n-1)%26)+res
+            n = (n-1)/26
+        return res
