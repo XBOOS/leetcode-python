cdb360fd627f9058243f8c2377f7bd68152098e6
xubing <xubinghku@gmail.com>
2016-02-05 18:22:50 +0800

    Feb 5 2016 Way back home for new year, and 100 for leetcodegit add .

--- best_time_to_sellAndBuy_stock_with_cooldown.py
+++ best_time_to_sellAndBuy_stock_with_cooldown.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
 
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]"""
+
+""" Method 1. Idea based on the best time to sell and buy stock II.
+when have to cool down, just compare the last addition,present addition, and two consecutive addtion plus the negation in between"""
 class Solution(object):
     def maxProfit(self, prices):
         """
