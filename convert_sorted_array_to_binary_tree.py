70b6a4e1d5fc7a6e1307e4bb5c52cdbfa770e059
xubing <xubinghku@gmail.com>
2016-02-22 16:25:54 +0800

    Feb 22 2016

--- convert_sorted_array_to_binary_tree.py
+++ convert_sorted_array_to_binary_tree.py
@@ -20,11 +20,36 @@ class Solution(object):
         length = len(nums)
         if length<1:
             return None
-        elif length==1:
-            return TreeNode(nums[0])
         else:
             middle = length/2
             root = TreeNode(nums[middle])
             root.left = self.sortedArrayToBST(nums[:middle])
             root.right = self.sortedArrayToBST(nums[middle+1:])
+            return root# Definition for a binary tree node.
+
+
+""" Method 1 is not quick enough, maybe due to the continuous copy of the array, i should just pass the bound of the array
+Below is method 2 using helper function"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        return self.sortedArrayToBST_withrange(nums,0,len(nums)-1)
+    def sortedArrayToBST_withrange(self,nums,l,r):
+        if r-l<0:
+            return None
+        else:
+            middle = (l+r)/2
+            root = TreeNode(nums[middle])
+            root.left = self.sortedArrayToBST_withrange(nums,l,middle-1)
+            root.right = self.sortedArrayToBST_withrange(nums,middle+1,r)
             return root
