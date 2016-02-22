b02ef60e50f08d608e783c10695a2419e5dade7d
xubing <xubinghku@gmail.com>
2016-02-22 20:12:33 +0800

    Feb 22 2016

--- populating_next_right_pointers_in_each_node.py
+++ populating_next_right_pointers_in_each_node.py
@@ -91,3 +91,31 @@ class Solution(object):
             root.right.next = None
         self.dfs(root.left)
         self.dfs(root.right)
+
+
+""" Method 3. Iterative method"""
+# Definition for binary tree with next pointer.
+# class TreeLinkNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        if not root:
+            return
+
+        while root.left:
+            parent = root
+            while parent:
+                parent.left.next = parent.right
+                if parent.next:
+                    parent.right.next = parent.next.left
+                parent = parent.next
+            root = root.left
