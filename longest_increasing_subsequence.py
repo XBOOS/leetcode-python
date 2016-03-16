9e7a1c88c7a896a1eb6a44fc1f50b290d6458a82
xubing <xubinghku@gmail.com>
2016-03-16 15:37:03 +0800

    Mar 16 2016

--- longest_increasing_subsequence.py
+++ longest_increasing_subsequence.py
@@ -36,5 +36,86 @@ class Solution(object):
 
         return max(maxs)
 
+
+""" A little bit optimization of the corner case detection"""
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        leng = len(nums)
+        maxs = [1]*leng
+
+        for i in range(1,leng):
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    maxs[i] = max(maxs[i],maxs[j]+1)
+
+        return max(maxs) if maxs else 0
 """
-Method 2 """
+Method 2. From others. Really brilliant idea.
+in method 1 some of the traversal time is wasted. because we maintained some information we no more need.
+only when the encounter element is lareger than all the previous ones, we increase the length by 1.
+we maintain a sorted list.always update it with smallest at the particular point of the array.
+Using binary search to locate the updating position. so the time complexity is O(nlog(n))"""
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums==[]:
+            return 0
+        leng = len(nums)
+        tool = []
+
+        for i in range(leng):
+            low = 0
+            high = len(tool)-1
+            while low<=high:
+                mid = (low+high)/2
+                if tool[mid]==nums[i]:
+                    break
+                elif tool[mid]>nums[i]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            if low<=high:
+                continue
+            if low>=len(tool):
+                tool.append(nums[i])
+            else:
+                tool[low] = nums[i]
+
+        return len(tool)
+
+""" Also could modify like this to deal with the exact found situation"""
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums==[]:
+            return 0
+        leng = len(nums)
+        tool = []
+
+        for i in range(leng):
+            low = 0
+            high = len(tool)-1
+            while low<=high:
+                mid = (low+high)/2
+                if tool[mid]>=nums[i]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            if low>=len(tool):
+                tool.append(nums[i])
+            else:
+                tool[low] = nums[i]
+
+        return len(tool)
+
