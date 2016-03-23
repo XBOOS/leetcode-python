eed166a0d44f82fec6e7ac3e54de832d4e541f95
xubing <xubinghku@gmail.com>
2016-03-23 16:21:54 +0800

    Mar 23 2016

--- bitwise_and_of_numbers_range.py
+++ bitwise_and_of_numbers_range.py
@@ -21,3 +21,17 @@ class Solution(object):
             mask |=(1<<k)
             k+=1
         return m&mask
+
+
+
+class Solution:
+    # @param m, an integer
+    # @param n, an integer
+    # @return an integer
+    def rangeBitwiseAnd(self, m, n):
+        p = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            p += 1
+        return m << p
