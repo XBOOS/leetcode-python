7b341bd08609b896a298a70d3d7d3f4122c47aea
xubing <xubinghku@gmail.com>
2016-03-26 15:48:23 +0800

    Mar 26 2016

--- root_to_leaf_numbers.py
+++ root_to_leaf_numbers.py
@@ -19,6 +19,12 @@ The root-to-leaf path 1->3 represents the number 13.
 Return the sum = 12 + 13 = 25.
 
 """
+
+""" Main idea is to pass the accumulated information to its left and right children
+Three are some options:
+1. modify the child's val add parent's information to the val
+2. pass it as an argument of recursion call
+3. add an extra field to the treeNode"""
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -44,3 +50,32 @@ class Solution(object):
             root.right.val+=root.val*10
             val+= self.sumNumbers(root.right)
         return val
+
+
+
+    """ Option2"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumNumbers(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def sums(root,prev):
+            if not root:
+                return 0
+            if not root.left and not root.right:
+                return prev*10+root.val
+            val = 0
+            if root.left:
+                val +=sums(root.left,prev*10+root.val)
+            if root.right:
+                val +=sums(root.right,prev*10+root.val)
+            return val
+        return sums(root,0)
