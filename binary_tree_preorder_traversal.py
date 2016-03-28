a56f13214459b2adf75fcb59d3ff68a21f14505e
xubing <xubinghku@gmail.com>
2016-03-28 17:14:17 +0800

    Mar 28 2016

--- binary_tree_preorder_traversal.py
+++ binary_tree_preorder_traversal.py
@@ -71,4 +71,33 @@ class Solution(object):
 
 
 
+""" Method 3 a good non-recursive solution"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = list()
+        stack = []
+        while root or stack:
+            if root:
+                res.append(root.val)
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                root = root.right
+        return res
+
+
 
