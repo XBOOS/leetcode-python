5ac2663e8b5d6b5ef508fd0d038fc37f716ba1d3
xubing <xubinghku@gmail.com>
2016-03-28 17:15:56 +0800

    Mar 28 2016

--- binary_tree.inorder_traversal.py
+++ binary_tree.inorder_traversal.py
@@ -151,3 +151,33 @@ class Solution(object):
 
 
 
+""" Method 4 a good solution"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        stack = []
+        res = list()
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                res.append(root.val)
+                root = root.right
+        return res
+
+
+
