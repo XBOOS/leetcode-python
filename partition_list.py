d049f2d61804af802b1f69c6039df9fcd44414bb
xubing <xubinghku@gmail.com>
2016-03-30 13:43:45 +0800

    Mar 30 2016

--- partition_list.py
+++ partition_list.py
@@ -11,6 +11,41 @@ Given 1->4->3->2->5->2 and x = 3,
 return 1->2->2->4->3->5.
 
 """
+
+""" Method1 using """
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        # dummy = ListNode(0)
+        # dummy.next = head
+
+        smallHead = smallCur = ListNode(0)
+        bigHead = bigCur = ListNode(0)
+
+        while head:
+            if head.val<x:
+                smallCur.next = head
+                smallCur = smallCur.next
+            else:
+                bigCur.next = head
+                bigCur = bigCur.next
+            head = head.next
+        smallCur.next = bigHead.next
+        return smallHead.next
+
+
+
+
 """ TLE"""
 # Definition for singly-linked list.
 # class ListNode(object):
