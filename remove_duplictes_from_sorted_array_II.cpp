c003917789620a75ab49659229484b1669b37dd0
xubing <xubinghku@gmail.com>
2016-04-03 08:07:58 +0800

    Apr 3 2016

--- remove_duplictes_from_sorted_array_II.cpp
+++ remove_duplictes_from_sorted_array_II.cpp
@@ -8,7 +8,9 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 */
 
-
+/* Method1
+ * intuitive method
+ */
 
 class Solution {
 public:
@@ -29,6 +31,24 @@ public:
                 prePrev = nums[slow-1];
 
             }
+        }
+        return slow;
+    }
+};
+
+/* Method 2.
+ * Focus on the newly created array. so just make constraints with slow
+ * in stead of fast.
+ */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<3) return nums.size();
+        int slow = 2;
+        for(int fast=2;fast<nums.size();++fast)
+        {
+            if(nums[fast]!=nums[slow-2])
+                nums[slow++] = nums[fast];
         }
         return slow;
     }
