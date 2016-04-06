e345cf7416440d9b2f49976feb3ef3d8c669d7c7
xubing <xubinghku@gmail.com>
2016-04-06 14:27:09 +0800

    Apr 6 2016

--- convert_sorted_list_to_binary_search_tree.cpp
+++ convert_sorted_list_to_binary_search_tree.cpp
@@ -55,3 +55,100 @@ public:
         return slow;
     }
 };
+
+/* Method 2 instead of spliting the original list to many sub-lists, we just
+ * change the nullptr detection to A ListNode "afterLast".
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+       return sortedListToBSTHelper(head,nullptr);
+
+    }
+    TreeNode* sortedListToBSTHelper(ListNode* head,ListNode* afterLast){
+        if(head==afterLast) return nullptr;
+        ListNode* mid = findMiddle(head,afterLast);
+        TreeNode* root = new TreeNode(mid->val);
+        root->left = sortedListToBSTHelper(head,mid);
+        root->right = sortedListToBSTHelper(mid->next,afterLast);
+        return root;
+
+    }
+    ListNode* findMiddle(ListNode* head,ListNode* afterLast){
+        if(head==afterLast||head->next==afterLast) return head;
+        // if(!head->next->next){
+        //     ListNode* middle = head->next;
+        //     head->next = NULL;
+        //     return middle;
+        // }
+        ListNode* slow = head;
+        ListNode* fast = head;
+      //  ListNode* prevSlow;
+        while(fast!=afterLast&&fast->next!=afterLast){
+           // prevSlow = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        //prevSlow->next = NULL;
+        return slow;
+    }
+};
+
+/*or just eliminate the helper function*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+       return sortedListToBSTHelper(head,nullptr);
+
+    }
+    TreeNode* sortedListToBSTHelper(ListNode* head,ListNode* afterLast){
+        if(head==afterLast) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=afterLast&&fast->next!=afterLast){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        TreeNode* root = new TreeNode(slow->val);
+        root->left = sortedListToBSTHelper(head,slow);
+        root->right = sortedListToBSTHelper(slow->next,afterLast);
+        return root;
+
+    }
+
+
+};
