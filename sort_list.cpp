f04f1b2fab54348acd57db3a7e9b70b0de1316e9
xubing <xubinghku@gmail.com>
2016-04-06 08:38:19 +0800

    Apr 6 2016

--- sort_list.cpp
+++ sort_list.cpp
@@ -1,6 +1,10 @@
 /*
  *Sort a linked list in O(n log n) time using constant space complexity.
+ *
+ * The classic sorting methods with time complexity of O(nlogn) are
+ * merge_sort,quick_sort.heap_sort
  */
+
 
 
 /**
@@ -15,7 +19,7 @@
 /*
  * Method1. Recursive merge sort
  * helper functions: mergeList(),splitList()::return the head of the seconde one(i.e.findMiddleNode)
- *
+ * THe merge sort for array need extra linear space for merging. but here is linked list. so that it dont need to use extra space.
  */
 class Solution {
 public:
@@ -65,4 +69,68 @@ private:
         return fakeHead.next;
     }
 
+};
+
+
+/*
+ * Mathod2 Recuisive Quick sort
+ * Partition + concate the small part,middle part and big part
+ * Be careful to set the tail to NULL to split the list during partition!
+ * and in the concate where to start the while loop.use walk->next, we can arrive at the tail of the list
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        return quickSort(head);
+    }
+    ListNode* quickSort(ListNode* head){
+        if(!head||!head->next) return head;
+        ListNode smallHead = ListNode(-1);
+        ListNode midHead = ListNode(-1);
+        ListNode bigHead = ListNode(-1);
+        ListNode* small = &smallHead;
+        ListNode* big = &bigHead;
+        ListNode* mid = &midHead;
+        int pivot = head->val;
+        while(head){
+            if(head->val<pivot){
+                small->next = head;
+                small = small->next;
+            }else if(head->val==pivot){
+                mid->next = head;
+                mid = mid->next;
+            }else{
+                big->next = head;
+                big = big->next;
+            }
+            head = head->next;
+        }
+        small->next = NULL;
+        mid->next = NULL;
+        big->next = NULL;
+        return concate(quickSort(smallHead.next),midHead.next,quickSort(bigHead.next));
+    }
+    ListNode* concate(ListNode* small,ListNode* mid,ListNode* big){
+        ListNode dummy = ListNode(-1);
+        ListNode* walk = &dummy;
+        walk->next = small;
+        while(walk->next){
+            walk = walk->next;
+        }
+        walk->next = mid;
+        while(walk->next){
+            walk = walk->next;
+        }
+        walk->next = big;
+
+        return dummy.next;
+    }
 };
