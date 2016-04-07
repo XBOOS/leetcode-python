e287862b16d36e2b6880d9ff8ded2e6ca49835f7
xubing <xubinghku@gmail.com>
2016-04-07 10:49:01 +0800

    Apr 7 2016

--- faltten_binary_tree_to_linked_list.py
+++ faltten_binary_tree_to_linked_list.py
@@ -51,3 +51,31 @@ class Solution(object):
             root = root.right
         root.right = tmp
 
+""" Method2 Brilliant iterative solution by others """
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        if not root: return root
+        stack = list()
+        prev = None
+        while root or stack:
+            if root.left:
+                if root.right:
+                    stack.append(root.right)
+                root.right,root.left = root.left,None
+            if not root.right and stack:
+                root.right = stack.pop()
+            root = root.right
+
+
+
