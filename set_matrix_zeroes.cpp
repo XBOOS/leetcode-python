df18eda93d943167fd427a804c443c682b5a34db
xubing <xubinghku@gmail.com>
2016-04-08 13:49:47 +0800

    Apr 8 2016

--- set_matrix_zeroes.cpp
+++ set_matrix_zeroes.cpp
@@ -46,6 +46,7 @@ public:
 /* Method2 using O(m+n) extra space
  * firstly select out the rows and columns which should be set to zeros then set them
  */
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -69,6 +70,58 @@ public:
         for(int col:cols){
             for(int k=0;k<m;++k) matrix[k][col]=0;
         }
+    }
+
+};
+
+/* Method3 O(1) space. using the first row and first column to be the the marker storage
+ */
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if(!m) return;
+        int n = matrix[0].size();
+        if(!n) return;
+        bool flag_row = false;
+        bool flag_col = false;
+        for(int i=0;i<m;++i){
+            if(!matrix[i][0]){
+                flag_col = true;
+                break;
+            }
+        }
+        for(int j=0;j<n;++j){
+            if(!matrix[0][j]){
+                flag_row = true;
+                break;
+            }
+        }
+        for(int i=1;i<m;++i){
+            for(int j=1;j<n;++j){
+                if(!matrix[i][j]){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;++i){
+            if(!matrix[i][0]) std::fill(matrix[i].begin(),matrix[i].end(),0);
+        }
+        for(int j=1;j<n;++j){
+            if(!matrix[0][j]){
+                for(int k=0;k<m;++k) matrix[k][j]=0;
+            }
+        }
+        if(flag_row) std::fill(matrix[0].begin(),matrix[0].end(),0);
+        if(flag_col){
+
+                for(int k=0;k<m;++k) matrix[k][0]=0;
+
+        }
+
+
     }
 
 };
