22ed74ee5ca928c80c2488efc4b824e24d209264
xubing <xubinghku@gmail.com>
2016-04-13 08:36:15 +0800

    Apr 13 2016

--- majority_element_II.cpp
+++ majority_element_II.cpp
@@ -41,3 +41,33 @@ public:
         return res;
     }
 };
+
+/* Method 2 quick selection recursive call*/
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> res;
+        if(nums.size()<2) return nums;
+        duplicated_quick_select(nums,0,nums.size()-1,nums.size()/3,res);
+        return res;
+    }
+
+    void duplicated_quick_select(vector<int>& nums,int low,int high,int len,vector<int>& ans){
+        if(low>high) return;
+        int pivot = nums[low];//or to safely get the mid and swap it with the first element
+        int i = low+1, k = low, j=low+1;
+        while(i<=high){
+            if(nums[i]<pivot){
+                swap(nums[i],nums[k++]);
+                swap(nums[i++],nums[j++]);
+            }else if(nums[i]==pivot){
+                swap(nums[i++],nums[j++]);
+            }else{
+                i++;
+            }
+        }
+        if(j-k>len) ans.push_back(nums[k]);
+        if(k-low>len) duplicated_quick_select(nums,low,k-1,len,ans);
+        if(high-j+1>len) duplicated_quick_select(nums,j,high,len,ans);
+    }
+};
