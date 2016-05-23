eb14aa0c9b0028bfe54ab0f1acf59959041027b3
xubing <xubinghku@gmail.com>
2016-05-23 22:45:17 +0800

    May 23 2016

--- gas_station.cpp
+++ gas_station.cpp
@@ -90,3 +90,24 @@ public:
         return (total<0)?-1:idx%n;
     }
 };
+
+
+//method 2
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        //vector<int> remain(n,0);
+        int total=0,sum=0,idx=0;
+        for(int i=0;i<n;++i){
+            sum+=(gas[i]-cost[i]);
+            if(sum<0){
+                 total+=sum;
+                 sum=0;
+                 idx=i+1;
+            }
+        }
+        total+=sum;
+        return (total<0)?-1:idx%n;
+    }
+};
