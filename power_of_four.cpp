f0432c049323e3c422b82519153cbe9651c32af9
xubing.xb <xubing.xb@cainiao.com>
2017-01-06 12:51:26 +0800

    Revert "May 24 2016"
    This reverts commit 64d9ffa5ba7c33ad76292f5ec927fbc971793a63.
    
    merge

--- power_of_four.cpp
+++ power_of_four.cpp
@@ -5,39 +5,8 @@ public:
     bool isPowerOfFour(int num) {
         int n=1;
         while(n<num){
-           n<<=2;
+            n>>2;
         }
         return n==num;
     }
-};
-
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        if(!num) return false;
-        while((num&3)==0) num=(num>>2);
-        return num==1;
-    }
-
-
-};
-
-//method 2
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        return (num&(num-1))==0 && (num&0x55555555)!=0;
-    }
-
-
-};
-
-//method 3
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        return (num&(num-1))==0 && (num-1)%3==0;
-    }
-
-
 };
